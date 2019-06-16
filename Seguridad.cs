73546b9ca665719f193858f63c0d86b835033c41
carlamendez <50938929+carlamendez@users.noreply.github.com>
2019-06-16 02:00:37 -0500

    Add files via upload

---
+++ Seguridad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commons
+{
+    public static class Seguridad
+    {
+        public static string Encriptar (string cadena)
+        {
+            string result = string.Empty;
+            byte[] encryted = System.Text.Encoding.Unicode.GetBytes(cadena);
+            result = Convert.ToBase64String(encryted);
+            return result;
+        }
+    }
+}
